0797326033aef7bf18cb58f931d5db6ab05c15c5
Byron Dominguez <bdominguez@MacBook-Pro-de-Timmy.local>
2020-09-19 14:48:27 -0500

    feat(front): :sparkles: add layout

--- src/layout/CalendarHome/index.tsx
+++ src/layout/CalendarHome/index.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react'
 import { CalendarContextConsumer } from '../../CalendarContext'
+import { Container, Row, Col } from 'reactstrap'
 
-export const CalendarHome = () => (
+export const CalendarHome = ({ children }) => (
   <CalendarContextConsumer>
     {(appContext) =>
       appContext && (
-        <div>
-          Name: {appContext.name}, Author: {appContext.author}, Url:{' '}
-          {appContext.url}
-        </div>
+        <Container>
+          <Row>
+            <Col>
+              Name: {appContext.name}, Author: {appContext.author}, Url:{' '}
+              {appContext.url}
+              {children}
+            </Col>
+          </Row>
+        </Container>
       )
     }
   </CalendarContextConsumer>
